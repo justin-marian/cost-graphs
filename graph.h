bddbe66b7d84d0752d4f0c7a70b0f46d7cee2320
Justin-Marian Popescu <pmarianjustin@gmail.com>
2024-03-08 15:11:22 +0200

    Added header graph file.

---
+++ graph.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#ifndef GRAPHTYPES_H
+#define GRAPHTYPES_H
+
+#include <vector>
+#include <limits>
+#include <unordered_map>
+#include <unordered_set>
+
+// Unreachable distance
+constexpr int INF = std::numeric_limits<int>::max();
+
+struct Node {
+    int id;
+    Node(int id) : id(id) {}
+};
+
+typedef std::pair<int, int> edge;
+typedef std::vector<std::vector<edge>> adjiacent_weight_map;
+typedef std::unordered_map<int, std::vector<int>> adjiacent_map;
+
+#endif // GRAPHTYPES_H
